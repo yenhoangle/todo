548d01ba1398ce003276f6274aebabedc8e4e3c7
Yen Le <yenhgle@gmail.com>
2020-05-22 03:33:47 -0700

    adding todo items work

---
+++ app.js
@@ -0,0 +1,38 @@
+//selectors
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+//event listeners
+todoButton.addEventListener('click', addTodo);
+
+//functions
+function addTodo(event) {
+    //prevent form from submitting
+    event.preventDefault();
+    //classList is used to manip class list
+    //create DIV
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+    //create LI
+    const newTodo = document.createElement('li');
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo); //making the new todo a child of todoDiv
+    //check mark button
+    const completedButton = document.createElement('button');
+    //use innerHTML instead of innerText to add icons
+    completedButton.innerHTML = '<i class= "fas fa-check"></i>';
+    completedButton.classList.add('complete-btn');
+    todoDiv.appendChild(completedButton);
+
+    //check trash button
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class= "fas fa-trash"></i>';
+    trashButton.classList.add('trash-btn');
+    todoDiv.appendChild(trashButton);
+    //append to list
+    todoList.appendChild(todoDiv);
+    //clear todo input value after appending
+    todoInput.value = "";
+}
