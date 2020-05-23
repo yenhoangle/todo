c5eb9fc7d751acf27b27c75a60aed4286bda3d2f
Yen Le <yenhgle@gmail.com>
2020-05-22 22:33:14 -0700

    deleting and checking todo items worked

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const todoList = document.querySelector('.todo-list');
 
 //event listeners
 todoButton.addEventListener('click', addTodo);
+todoList.addEventListener('click', deleteCheck);
 
 //functions
 function addTodo(event) {
@@ -35,4 +36,20 @@ function addTodo(event) {
     todoList.appendChild(todoDiv);
     //clear todo input value after appending
     todoInput.value = "";
+}
+
+function deleteCheck(e) {
+    const item = e.target;
+    //delete todo
+    if(item.classList[0] === 'trash-btn') {
+        //remove parent element of the trash button
+        const todo = item.parentElement;
+        todo.remove();
+    }
+
+    //check mark
+    if(item.classList[0] === 'complete-btn') {
+        const todo = item.parentElement;
+        todo.classList.toggle("completed");
+    }
 }
